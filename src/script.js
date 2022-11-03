360bf8106151d6592b25e9c24d1a12932ce147d5
Pratik Jadhav <96625125+Pratik-Jadhav7@users.noreply.github.com>
2022-11-03 08:50:09 +0530

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const name = "Pratik";
+const fname = "Pratik";
+const lname = "Jadhav";
 
 ReactDOM.render(
   <div>
-  <h1>My Favorite Food by {name}</h1>
+  <h1>My Favorite Food by {fname} {lname}</h1>
   <ul>
     <li>Dosa</li>
     <li>Idli</li>
