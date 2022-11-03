c0545d4f8d3f01004a81d3220d97546d9e9486a8
Pratik Jadhav <96625125+Pratik-Jadhav7@users.noreply.github.com>
2022-11-03 08:49:05 +0530

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,4 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello WOrld!!!</h1>, document.getElementById("root"));
+const name = "Pratik";
+
+ReactDOM.render(
+  <div>
+  <h1>My Favorite Food by {name}</h1>
+  <ul>
+    <li>Dosa</li>
+    <li>Idli</li>
+    <li>Uttapa</li>
+  </ul>
+
+  </div>,
+ 
+  document.getElementById("root")
+);
