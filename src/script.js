3c4bed40c75dca4a1460f8a891b68c4e7a32baac
Pratik Jadhav <96625125+Pratik-Jadhav7@users.noreply.github.com>
2022-11-05 11:35:54 +0530

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,19 +1,1 @@
-import React from "react";
-import ReactDOM from "react-dom";
 
-const fname = "Pratik";
-const lname = "Jadhav";
-
-ReactDOM.render(
-  <div>
-  <h1>My Favorite Food by {fname} {lname}</h1>
-  <ul>
-    <li>Dosa</li>
-    <li>Idli</li>
-    <li>Uttapa</li>
-  </ul>
-
-  </div>,
- 
-  document.getElementById("root")
-);
