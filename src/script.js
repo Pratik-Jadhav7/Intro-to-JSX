ab7d21eefa299454a28489e410858758662ba970
Pratik Jadhav <96625125+Pratik-Jadhav7@users.noreply.github.com>
2022-11-06 17:03:23 +0530

    Update script.js

--- src/script.js
+++ src/script.js
@@ -79,6 +79,26 @@ ReactDOM.render(
 
 import React from "react";
 import ReactDOM from "react-dom";
+
+const date = new Date();
+const currentTime = date.getHours();
+
+const customStyle = {
+  color: ""
+}
+
+let greeting;
+
+if(currentTime < 12){
+  greeting = "Good Morning"
+  customStyle.color = "red";
+} else if(currentTime > 12 && currentTime < 18){
+  greeting = "Good Afternoon"
+  customStyle.color = "green";
+} else {
+  greeting = "Good Evening"
+  customStyle.color = "blue";
+}
 ReactDOM.render(
   <div>
     <h1 className= "heading " style={customStyle}>{greeting}</h1>
